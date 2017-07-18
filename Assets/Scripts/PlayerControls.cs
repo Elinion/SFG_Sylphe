5b5ce3dfea04bca514434490de9ca29c035c23e6
alexandre <alexandre.abts@gmail.com>
2017-07-18 21:24:00 +0400

    [playerControls] move the player by swiping

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -4,37 +4,63 @@ using UnityEngine;
 
 public class PlayerControls : MonoBehaviour
 {
+	public float moveSensitivity = 1f;
+	public float maxHorizontalSpeed = 5;
+	public float maxVerticalSpeed = 5;
+
 	private Vector2 touchOrigin = -Vector2.one;
+	private Rigidbody playerRigidbody;
+	private Vector2 swipeInput = Vector2.zero;
+
+	void Awake ()
+	{
+		playerRigidbody = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate ()
+	{
+		MovePlayer (swipeInput);
+	}
 
 	void Update ()
 	{
-		int horizontal = 0;
-		int vertical = 0;
+		swipeInput = GetSwipeInput ();
+	}
+
+	private void ApplySpeedLimits ()
+	{
+		Vector3 playerVelocity = playerRigidbody.velocity;
+		if (Mathf.Abs (playerVelocity.x) > maxHorizontalSpeed) {
+			playerVelocity.x = playerVelocity.x > 0 ? maxHorizontalSpeed : -maxHorizontalSpeed;
+		}
+		if (Mathf.Abs (playerVelocity.y) > maxVerticalSpeed) {
+			playerVelocity.y = playerVelocity.y > 0 ? maxVerticalSpeed : -maxVerticalSpeed;
+		}
+		playerRigidbody.velocity = playerVelocity;
+	}
+
+	private Vector2 GetSwipeInput ()
+	{
+		float horizontalMove = 0f;
+		float verticalMove = 0f;
 		if (Input.touchCount > 0) {
 			Touch touch = Input.touches [0];
 			if (touch.phase == TouchPhase.Began) {
 				touchOrigin = touch.position;
 			} else if (touch.phase == TouchPhase.Ended && touchOrigin.x >= 0) {
 				Vector2 touchEnd = touch.position;
-				float horizontalMove = touchEnd.x - touchOrigin.x;
-				float verticalMove = touchEnd.y - touchOrigin.y;
+				horizontalMove = touchEnd.x - touchOrigin.x;
+				verticalMove = touchEnd.y - touchOrigin.y;
 				touchOrigin.x = -1;
-				if (Mathf.Abs (horizontalMove) > Mathf.Abs (verticalMove)) {
-					horizontal = horizontalMove > 0 ? 1 : -1;
-				} else {
-					vertical = verticalMove > 0 ? 1 : -1;
-				}
 			}
 		}
+		return new Vector2 (horizontalMove, verticalMove);
+	}
 
-		if (horizontal == 1) {
-			Debug.Log ("Swipe right");
-		} else if (horizontal == -1) {
-			Debug.Log ("Swipe left");
-		} else if (vertical == 1) {
-			Debug.Log ("Swipe up");
-		} else if (vertical == -1) {
-			Debug.Log ("Swipe down");
-		}
+	private void MovePlayer (Vector2 swipeMove)
+	{
+		Vector3 moveForce = -swipeMove.normalized * moveSensitivity;
+		playerRigidbody.AddForce (moveForce);
+		ApplySpeedLimits ();
 	}
 }
