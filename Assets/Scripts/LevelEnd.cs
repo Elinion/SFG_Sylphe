7268750fcfe591699e3994bfadfa748145864f17
alexandre <alexandre.abts@gmail.com>
2017-07-19 12:55:35 +0400

    [level] added level end + restart level button

---
+++ Assets/Scripts/LevelEnd.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelEnd : MonoBehaviour
+{
+	public GameObject levelEndHUD;
+
+	void Awake ()
+	{
+		levelEndHUD.SetActive (false);
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.gameObject.tag == Tags.Player) {
+			levelEndHUD.SetActive (true);
+		}
+	}
+}
