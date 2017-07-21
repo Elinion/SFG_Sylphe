c348e3f6c7ba832818fd9b5a35a84976db7eb1c1
alexandre <alexandre.abts@gmail.com>
2017-07-21 10:39:13 +0400

    [score] added a sparkling trail to the ball

---
+++ Assets/Scripts/PlayerBall.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBall : MonoBehaviour
+{
+	private Rigidbody2D playerRigidbody;
+
+	void Awake ()
+	{
+		playerRigidbody = GameObject.FindGameObjectWithTag (Tags.Player).GetComponent<Rigidbody2D> ();
+	}
+
+	void Update ()
+	{
+		Vector2 velocity = playerRigidbody.velocity * -1;
+		transform.rotation = Quaternion.LookRotation (velocity);
+	}
+
+}
