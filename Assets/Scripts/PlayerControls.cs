705f640e136d8ff19e9ad6e39d3df35cef229fee
alexandre <alexandre.abts@gmail.com>
2017-07-24 12:41:45 +0400

    [playerControls] improved by integrating swipe speed

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class PlayerControls : MonoBehaviour
 {
 	public float constantForwardMoveForce = 50f;
-	public float maxSwipeForce = 50f;
-	public float maxSwipeLength = 600f;
-	public float maxHorizontalSpeed = 5;
-	public float maxVerticalSpeed = 5;
+	public float moveForce = 50f;
+	public float maxHorizontalSpeed = 5f;
+	public float maxVerticalSpeed = 5f;
 
 	private Vector2 touchOrigin = -Vector2.one;
 	private Rigidbody2D playerRigidbody;
+	private float swipeStartTime = 0f;
+	private float swipeEndTime = 0f;
+	private Vector2 swipeInput = Vector2.zero;
+	private bool hasUnhandledSwipe = false;
 
 	void Awake ()
 	{
@@ -20,8 +23,17 @@ public class PlayerControls : MonoBehaviour
 
 	void FixedUpdate ()
 	{
-		Vector2 input = HandleInput ();
+		Vector2 input = Vector2.zero;
+		if (hasUnhandledSwipe) {
+			input = swipeInput;
+			hasUnhandledSwipe = false;
+		}
 		MovePlayer (input);
+	}
+
+	void Update ()
+	{
+		HandleInput ();
 	}
 
 	private void ApplySpeedLimits ()
@@ -40,55 +52,50 @@ public class PlayerControls : MonoBehaviour
 	{
 		Vector2 input = Vector2.zero;
 		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
-			input.x = maxSwipeLength * .5f;
+			input.x = 300;
 		} 
 		if (Input.GetKeyDown (KeyCode.RightArrow)) {
-			input.x = -maxSwipeLength * .5f;
+			input.x = -300;
 		}
 		return input;
 	}
 
 	private Vector2 GetSwipeInput ()
 	{
-		float horizontalMove = 0f;
-		float verticalMove = 0f;
+		Vector2 swipe = Vector2.zero;
 		if (Input.touchCount > 0) {
 			Touch touch = Input.touches [0];
 			if (touch.phase == TouchPhase.Began) {
 				touchOrigin = touch.position;
+				swipeStartTime = Time.time;
 			} else if (touch.phase == TouchPhase.Ended) {
-				horizontalMove = touch.position.x - touchOrigin.x;
-				verticalMove = touch.position.y - touchOrigin.y;
+				swipe.x = touch.position.x - touchOrigin.x;
+				swipe.y = touch.position.y - touchOrigin.y;
 				touchOrigin = -Vector2.one;
+				swipeEndTime = Time.time;
+				float swipeTime = (swipeEndTime - swipeStartTime) * 1000f;
+				float swipeLength = swipe.magnitude;
+				float swipeSpeed = swipeLength / swipeTime;
+				swipe = swipe.normalized;
+				swipe *= swipeSpeed;
+				hasUnhandledSwipe = true;
 			}
 		}
-		return new Vector2 (horizontalMove, verticalMove);
+		return swipe;
 	}
 
-	private Vector2 HandleInput ()
+	private void HandleInput ()
 	{
-		Vector2 input;
-
-		#if UNITY_ANDROID
-
-		input = GetSwipeInput ();
-
-		#else
-
-		input = GetKeyboardInput ();
-
-		#endif
-
-		return input;
+		if (!hasUnhandledSwipe) {
+			swipeInput = GetSwipeInput ();
+		}
 	}
 
-	private void MovePlayer (Vector2 swipeMove)
+	private void MovePlayer (Vector2 move)
 	{
-		float swipeLength = swipeMove.magnitude;
-		float moveSpeed = swipeLength * maxSwipeForce / maxSwipeLength;
-		Vector2 moveForce = -swipeMove.normalized * moveSpeed;
-		moveForce.y += constantForwardMoveForce;
-		playerRigidbody.AddForce (moveForce);
+		move *= -moveForce;
+		move.y += constantForwardMoveForce;
+		playerRigidbody.AddForce (move);
 		ApplySpeedLimits ();
 	}
 }
