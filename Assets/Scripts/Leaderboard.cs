f03eb76eed48d4e7dd2fc3adce1cd7354c2c9ac0
alexandre <alexandre.abts@gmail.com>
2017-07-25 16:19:18 +0400

    [HUD] added pause button icon and canvas scaler

--- Assets/Scripts/Leaderboard.cs
+++ Assets/Scripts/Leaderboard.cs
@@ -2,11 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SocialPlatforms;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
 
 public class Leaderboard : MonoBehaviour
 {
 	public void Show ()
 	{
-		Social.ShowLeaderboardUI ();
+		PlayGamesPlatform.Instance.LoadScores (
+			GPGSIds.leaderboard_general,
+			LeaderboardStart.PlayerCentered,
+			100,
+			LeaderboardCollection.Public,
+			LeaderboardTimeSpan.AllTime,
+			(data) => {
+			});
 	}
 }
